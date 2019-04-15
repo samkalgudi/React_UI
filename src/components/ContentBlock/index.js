a728212354940801e38db7bf930635956053fec7
Sameera Kalgudi <M1037388@mindtree.com>
2019-04-15 14:49:37 +0530

    first commit

---
+++ src/components/ContentBlock/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class ContentBlock extends Component {
+    render() {
+        return(
+            <div>YOOOOOOO!!</div>
+        )
+    }
+}
+export default ContentBlock;
